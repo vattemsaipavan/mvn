e520d2a4ea8dad51212ebbd954e97f90fbea9843
vattemsaipavan <vattemsaipavan@gmail.com>
2021-07-17 17:55:46 -0500

    adding code

---
+++ HelloWorldProject-1/src/main/java/com/example/demo/HelloWorldProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldProject1Application.class, args);
+	}
+
+}
